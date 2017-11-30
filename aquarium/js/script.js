63cb4bff49b9b3612a69a2ce5f3e58aab6a900ff
LivPasc <31960651+LivPasc@users.noreply.github.com>
2017-11-30 13:27:45 +0100

    fish
    upload

--- aquarium/js/script.js
+++ aquarium/js/script.js
@@ -1,7 +1,29 @@
-
+    move_bubble($("#bubble1Id"));
+    move_bubble($("#bubble2Id"));
+    move_bubble($("#bubble3Id"));
+    move_bubble($("#bubble4Id"));
 
 // Implement the aquarium dynamics in this file
 
+$(".bubbleClass").click(function(){
+        
+        $(this).stop();
+        
+        $(this).animate({"opacity": 0}).delay(100);
+        
+        $(this).animate({"left" : Math.floor(Math.random() * ($(window).width() - 120)) + "px", "top" : $(window).height() + Math.floor(Math.random() * 500)});
+        $('embed').remove();
+		$('body').append('<embed src="images/bubble.mp3" autostart="true" hidden="true" loop="false">');
+        $(this).animate({"opacity": 0.85}, function(){ move_bubble(this); });
+        
+    });
+
+function move_bubble(ref)
+    {
+        $(ref).css({"left" : Math.floor(Math.random() * ($(window).width() - 120)) + "px", "top" : $(window).height() + Math.floor(Math.random() * 400)});
+        
+        $(ref).animate({"top" : "-75px", "left" : "+=300px"} , 9000, "linear", function(){ move_bubble(ref) });
+    }
 
 
 $("#fish1Id").animate({left: "=10"}, function () {squareMovement(this)});
@@ -39,10 +61,17 @@ $(window).click(function(e) {
     });
 
 $("#fish1Id").dblclick(function(){
+    $("#fish1Id").stop();
+    $("#fish1Id").stop();
+    $("#fish1Id").stop();
+    $("#fish1Id").stop();
     
 $("#fish1Id").css("height","400px");
 $("#fish1Id").css("width","400px");
+$("#fish1Id").animate({left: "+=1"},function() {squareMovement(this)});
 setTimeout(backToNormalSize, 3000);
+    $('embed').remove();
+		$('body').append('<embed src="images/fish1.mp3" autostart="true" hidden="true" loop="false">');
 
 });
 
@@ -95,6 +124,8 @@ var windowHeight = $(window).height(); //retrieve current window height
     $("#fish2Id").animate({top:newHight-100, left: newWidth-100},1000);
     
     $("#fish2Id").animate({left: "+=1"},function() {squareMovement(this)});
+    $('embed').remove();
+		$('body').append('<embed src="images/fish2.mp3" autostart="true" hidden="true" loop="false">');
 
 });
 
